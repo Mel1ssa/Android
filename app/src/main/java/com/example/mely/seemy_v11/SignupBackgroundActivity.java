418933a03e649aa21bc5e48d0a8a70fec3894401
Nicolas Delalande <ni.delalande@laposte.net>
2017-05-05 20:46:12 +0200

    nouvelle adresse du serveur

--- app/src/main/java/com/example/mely/seemy_v11/SignupBackgroundActivity.java
+++ app/src/main/java/com/example/mely/seemy_v11/SignupBackgroundActivity.java
@@ -31,8 +31,8 @@ public class SignupBackgroundActivity extends AsyncTask {
         String email=     (String) objects[3];
         String sexe =     (String) objects[4];
 
-        String link = "http://10.127.209.87/android/inscription.php?Pseudo="+username+"&MotDePasse="+password+"&Email="+email+"&Age="+age+"&Sexe="+sexe;
-
+       // String link = "http://10.127.209.87/android/inscription.php?Pseudo="+username+"&MotDePasse="+password+"&Email="+email+"&Age="+age+"&Sexe="+sexe;
+        String link = "http://nicolasdke.cluster023.hosting.ovh.net/seemy/inscription_PDO.php?Pseudo="+username+"&MotDePasse="+password+"&Email="+email+"&Age="+age+"&Sexe="+sexe;
         try {
             URL url = new URL(link);
             HttpClient client = new DefaultHttpClient();
